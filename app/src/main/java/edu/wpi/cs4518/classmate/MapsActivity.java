298588912f6d01a56f29c9392ce8e214eef02eb6
Velkoz <silverstar1230000@gmail.com>
2017-02-25 16:51:13 -0500

    merged map fragment and added tab base

---
+++ app/src/main/java/edu/wpi/cs4518/classmate/MapsActivity.java
@@ -0,0 +1,54 @@
+package edu.wpi.cs4518.classmate;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    // TODO: Connect to server, get list of study locations
+    // From these locations, set markers using latlng
+    // Create handler for clicking a marker, that will request info from server
+    // (primary info needed for map is uid, name, lat, lng - detail needs all)
+    // (note for handlers initialize in class not just in onCreate)
+    // on the click, get info and then switch to detail activity, populate with info
+    // (pass the uid tag to the intent, uid is used to look up the visible name)
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
---
+++ app/src/main/java/edu/wpi/cs4518/classmate/MapsDetail.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs4518.classmate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MapsDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_detail);
+    }
+}
