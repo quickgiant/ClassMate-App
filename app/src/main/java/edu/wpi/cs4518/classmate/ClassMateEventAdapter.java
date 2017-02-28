a200c2e21a2efdd2d519e8fe9c8e3ff599dcb7f9
Clark Jacobsohn <clark.jacobsohn@gmail.com>
2017-02-27 23:14:41 -0500

    Added button click listeners to event list items

--- app/src/main/java/edu/wpi/cs4518/classmate/ClassMateEventAdapter.java
+++ app/src/main/java/edu/wpi/cs4518/classmate/ClassMateEventAdapter.java
@@ -1,11 +1,14 @@
 package edu.wpi.cs4518.classmate;
 
 import android.content.Context;
+import android.content.Intent;
+import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -32,7 +35,7 @@ public class ClassMateEventAdapter extends ArrayAdapter<ClassMateEvent> {
         TextView usersAttendingView = (TextView) view.findViewById(R.id.users_attending_view);
         TextView semanticLocationView = (TextView) view.findViewById(R.id.semantic_location_view);
 
-        ClassMateEvent event = getItem(position);
+        final ClassMateEvent event = getItem(position);
         if(event != null) {
             eventTitleView.setText(event.getTitle());
             dateView.setText(event.getDateString());
@@ -44,6 +47,34 @@ public class ClassMateEventAdapter extends ArrayAdapter<ClassMateEvent> {
             throw new NullPointerException("ClassMateEventAdapter missing event object at" +
             " position " + position);
         }
+
+        Button addToCalendarButton = (Button) view.findViewById(R.id.add_to_calendar_button_view);
+        addToCalendarButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleAddToCalendarClick(event);
+            }
+        });
+        Button attendingButton = (Button) view.findViewById(R.id.attending_button_view);
+        attendingButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleAttendingClick(event);
+            }
+        });
+
         return view;
+    }
+
+    public void handleAddToCalendarClick(ClassMateEvent event) {
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, event.getStartTime().getTime())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, event.getEndTime().getTime())
+                .putExtra(CalendarContract.Events.TITLE, event.getTitle())
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, event.getSemanticLocation());
+        getContext().startActivity(intent);
+    }
+
+    public void handleAttendingClick(ClassMateEvent event) {
+        // TODO: Increment attending and submit to server
     }
 }
